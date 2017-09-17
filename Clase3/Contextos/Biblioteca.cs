fa78915381efbcee870aaab41333049731e3ce5d
DanielCarbajal314 <daniel.carbajal@pucp.edu.pe>
2017-09-17 09:18:37 -0500

    first Commit

---
+++ Clase3/Contextos/Biblioteca.cs
@@ -0,0 +1,16 @@
+﻿using Clase3.Modelos;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase3.Contextos
+{
+    public class Biblioteca:DbContext
+    {
+        public Biblioteca() : base("BibliotecaDB") { }
+        public DbSet<Libro> Libros { get; set; }
+    }
+}
---
+++ ConsolaDeDatos/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Clase3.Contextos;
+using Clase3.Modelos;
+
+namespace ConsolaDeDatos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Biblioteca biblitoteca = new Biblioteca();
+            Libro nuevoLibro = new Libro();
+            nuevoLibro.Autor = "Daniel";
+            nuevoLibro.Edicion = 3;
+            nuevoLibro.Genero = "Terror";
+            nuevoLibro.Nombre = "Cibertac";
+            biblitoteca.Libros.Add(nuevoLibro);
+            biblitoteca.SaveChanges();
+        }
+    }
+}
